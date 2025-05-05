fc06ccdbdf31bf768b888fca9a992992d8ebadc4
Blade Hagman <124316962+Stattius@users.noreply.github.com>
2025-05-04 22:56:49 -0500

    implementing the main fully
    basically copied from in class example

--- Adopt_Me2/src/main/java/adopt_me2/mvc/Main.java
+++ Adopt_Me2/src/main/java/adopt_me2/mvc/Main.java
@@ -7,28 +7,18 @@ import adopt_me2.model.Pet_Model;
 import adopt_me2.view.Pet_View;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-				SwingUtilities.invokeLater(new Runnable() {
-					
-					@Override
-					public void run() {
-						
-						Pet_Controller controller = new Pet_Controller(
-								
-								new Pet_Model(),
-								new Pet_View());
-								controller.intiate();
-								
-								
-						
-						
-					}
-					
-				});
-	}
+        	SwingUtilities.invokeLater(new Runnable() {
+			
+            	@Override
+            	public void run() {
+                
+               		PetModel model = new PetModel();
+                	PetView view = new PetView();
+                	new PetController(model, view);
 
+                	view.display();
+            	}
+        });
+    }
 }
